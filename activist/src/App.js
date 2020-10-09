7f3baf4ba4974305ed387a73b3f5a4db1924ebd7
klsz <klsz@itu.dk>
2020-10-09 13:29:27 +0200

    css template

--- activist/src/App.js
+++ activist/src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './img/logo 1.png';
 import './App.css';
 
 function App() {
   return (
     <div className="ACTivist">
-      <header className="App-header">
+{/*      <header className="App-header"> */}
+        <div className="container">
+        
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+        </div>
+        
+  {/*    </header> */}
     </div>
   );
 }
