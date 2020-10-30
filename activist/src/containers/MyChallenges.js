48a329efab628e2dcdb16e0dedc371d6099d7da5
Louise Braun <louisebraun@Louises-MacBook-Pro.local>
2020-10-30 13:40:18 +0100

    mychallenges updated with components

--- activist/src/containers/MyChallenges.js
+++ activist/src/containers/MyChallenges.js
@@ -9,103 +9,114 @@ import Homebutton from "./components/homebutton";
 export const MyChallenges = () => {
   return (
     <main>
-      {/*to be changed by Louise as a component */}
-      <div>
-        <Link to="/settings">
-          <img
-            className="column"
-            src={set}
-            style={{ width: "5%", float: "right" }}
-            alt="set"
-          />
-        </Link>
-        <div>
-          <Link to="/home">
-            <img
-              className="column"
-              src={home2}
-              style={{ width: "5%", float: "left" }}
-              alt="set"
-            />
-          </Link>
-        </div>
+      <div id="navbuttons">
+        <Settingbutton />
+        <Homebutton />
       </div>
-
       <div className="text">
         <h3>Food</h3>
-        <div>
-          <button id="challbutton">
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                value=""
-                id="defaultCheck1"
-              />
-              <label className="form-check-label" htmlFor="defaultCheck1">
-                No beef!
-                <br /> <b id="challtext">Can save CO2: 160 kg/day</b>
-              </label>
-            </div>{" "}
-          </button>
-        </div>
-
-        <div>
-          <button id="challbutton">
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                value=""
-                id="defaultCheck1"
-              />
-              <label className="form-check-label" htmlFor="defaultCheck1">
-                Go pescatarian!
-                <br /> <b id="challtext">Can save CO2: 200 kg/day </b>
-              </label>
-            </div>{" "}
-          </button>
-        </div>
-
-        <div>
-          <button id="challbutton">
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                value=""
-                id="defaultCheck1"
-              />
-              <label className="form-check-label" htmlFor="defaultCheck1">
-                Go vegetarian!
-                <br /> <b id="challtext">Can save CO2: 250 kg/day </b>
-              </label>
-            </div>
-          </button>
-        </div>
-
-        <div>
-          <button id="challbutton">
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                value=""
-                id="defaultCheck1"
-              />
-              <label className="form-check-label" htmlFor="defaultCheck1">
-                Go vegan!
-                <br />
-                <b id="challtext">Can save CO2: 300 kg/day </b>
-              </label>
-            </div>
-          </button>
-        </div>
-
-        <div>
-          <button className="button2"> Pledge </button>
-        </div>
+        <CheckboxContainer />
       </div>
+      );
     </main>
   );
 };
+
+/*to be changed by Louise as a component 
+<div>
+<Link to="/settings">
+  <img
+    className="column"
+    src={set}
+    style={{ width: "5%", float: "right" }}
+    alt="set"
+  />
+</Link>
+<div>
+  <Link to="/home">
+    <img
+      className="column"
+      src={home2}
+      style={{ width: "5%", float: "left" }}
+      alt="set"
+    />
+  </Link>
+</div>
+</div>
+
+<div className="text">
+<h3>Food</h3>
+<div>
+  <button id="challbutton">
+    <div className="form-check">
+      <input
+        className="form-check-input"
+        type="checkbox"
+        value=""
+        id="defaultCheck1"
+      />
+      <label className="form-check-label" htmlFor="defaultCheck1">
+        No beef!
+        <br /> <b id="challtext">Can save CO2: 160 kg/day</b>
+      </label>
+    </div>{" "}
+  </button>
+</div>
+
+<div>
+  <button id="challbutton">
+    <div className="form-check">
+      <input
+        className="form-check-input"
+        type="checkbox"
+        value=""
+        id="defaultCheck1"
+      />
+      <label className="form-check-label" htmlFor="defaultCheck1">
+        Go pescatarian!
+        <br /> <b id="challtext">Can save CO2: 200 kg/day </b>
+      </label>
+    </div>{" "}
+  </button>
+</div>
+
+<div>
+  <button id="challbutton">
+    <div className="form-check">
+      <input
+        className="form-check-input"
+        type="checkbox"
+        value=""
+        id="defaultCheck1"
+      />
+      <label className="form-check-label" htmlFor="defaultCheck1">
+        Go vegetarian!
+        <br /> <b id="challtext">Can save CO2: 250 kg/day </b>
+      </label>
+    </div>
+  </button>
+</div>
+
+<div>
+  <button id="challbutton">
+    <div className="form-check">
+      <input
+        className="form-check-input"
+        type="checkbox"
+        value=""
+        id="defaultCheck1"
+      />
+      <label className="form-check-label" htmlFor="defaultCheck1">
+        Go vegan!
+        <br />
+        <b id="challtext">Can save CO2: 300 kg/day </b>
+      </label>
+    </div>
+  </button>
+</div>
+
+<div>
+  <button className="button2"> Pledge </button>
+</div>
+</div>
+*/
