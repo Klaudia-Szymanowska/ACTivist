f08808e2d3a0eeca45a212d079c0d4c876063daf
Laia <menc@itu.dk>
2020-12-01 10:32:54 +0100

    make welcome prettier 2

--- activist/src/containers/Welcome.js
+++ activist/src/containers/Welcome.js
@@ -31,10 +31,16 @@ export const Welcome = () => {
           </h3>
           <h3 >
             Every day the average Dane produces 47 kg of CO2. In order to reduce it, you can challenge yourself and see your progress on the next sreen using the three buttons: 
-            <p align="left"> <img src={chall} alt="chall" /> Choose and pledge to challenges. </p>
-            <p align='left'> <img src={list} alt="list" /> See your ongoing and finished challenges.</p>
-            <p align='left'> <img src={rew} alt="rew" /> See the statistics of your accomplishments.</p>
+            
+            <p align="left"> <div className="iconswelcome"> <img src={chall} alt="chall" />  </div> Choose and pledge to challenges. </p>
+          
+            
+            <p align='left'> <div className="iconswelcome"> <img src={list} alt="list" />  </div>See your ongoing and finished challenges.</p>
+           
+           
+            <p align='left'> <div className="iconswelcome">  <img src={rew} alt="rew" /> </div>See the statistics of your accomplishments.</p>
 
+            
           </h3>
           <h3>Let's save the climate together!</h3>
         </div>
